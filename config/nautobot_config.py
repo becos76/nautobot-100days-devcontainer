ebc5a8385e50a7641f098b212c70c0aaf7f5b2c0
Manolis <mkaliotis@gmail.com>
2025-08-16 00:33:24 +0300

    D41 - Installing Apps

--- config/nautobot_config.py
+++ config/nautobot_config.py
@@ -35,13 +35,46 @@ CACHEOPS_REDIS = parse_redis_connection(redis_database=1)
 
 # Enable installed plugins. Add the name of each plugin to the list.
 # PLUGINS = ["nautobot_example_plugin"]
-PLUGINS = ["nautobot_example_plugin"]
-
+# PLUGINS = ["nautobot_plugin_nornir", "nautobot_golden_config"]
+PLUGINS = []
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
-PLUGINS_CONFIG = {
-    "nautobot_example_plugin": {},
-}
+PLUGINS_CONFIG = {}
+# PLUGINS_CONFIG = {
+#     "nautobot_plugin_nornir": {
+#         "nornir_settings": {
+#             "credentials": "nautobot_plugin_nornir.plugins.credentials.env_vars.CredentialsEnvVars",
+#             "runner": {
+#                 "plugin": "threaded",
+#                 "options": {
+#                     "num_workers": 20,
+#                 },
+#             },
+#         },
+#     },
+#     "nautobot_golden_config": {
+#         "per_feature_bar_width": 0.15,
+#         "per_feature_width": 13,
+#         "per_feature_height": 4,
+#         "enable_backup": True,
+#         "enable_compliance": True,
+#         "enable_intended": True,
+#         "enable_sotagg": True,
+#         "enable_plan": True,
+#         "enable_deploy": True,
+#         "enable_postprocessing": False,
+#         "sot_agg_transposer": None,
+#         "postprocessing_callables": [],
+#         "postprocessing_subscribed": [],
+#         "jinja_env": {
+#             "undefined": "jinja2.StrictUndefined",
+#             "trim_blocks": True,
+#             "lstrip_blocks": False,
+#         },
+#         # "default_deploy_status": "Not Approved",
+#         # "get_custom_compliance": "my.custom_compliance.func"
+#     },
+# }
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080", "https://localhost:8080"] 
 
