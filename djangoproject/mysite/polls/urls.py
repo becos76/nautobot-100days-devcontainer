db3502099d6bbe28df89e44bbbd929c47926610c
Manolis <mkaliotis@gmail.com>
2025-08-16 21:54:16 +0300

    D49 - Django Example

---
+++ djangoproject/mysite/polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # Home page of the polls app
+    path('<int:question_id>/', views.detail, name='detail'),  # Detail page for a specific question
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
